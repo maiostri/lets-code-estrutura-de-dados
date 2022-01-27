6fab7406c71b4652040e2446587c07b0d53fd9c0
Ricardo Maiostri <maiostri@gmail.com>
2022-01-26 21:49:52 -0300

    Adicionando grafos.

---
+++ aula06/graph.js
@@ -0,0 +1,71 @@
+class Graph {
+
+    // Nosso grafo é um grafo sem peso
+    // Não vamos ter ciclos.
+    // Pode ser bi-direcional.
+
+    constructor() {
+        this.adjList = new Map();
+    }
+
+    // addVertex -> adiciona o vertice
+    // vertice nosso é um numero.
+    addVertex(vertice) {
+        this.adjList.set(vertice, []);
+    }
+
+    // addEdge -> adiciona a aresta
+    // origem e destino são outros vertices.
+    addEdge(origem, destino) {
+        // Adiciono o caminho para o destino.
+        this.adjList.get(origem).push(destino);
+    }
+
+    printGraph() {
+        // Pegar todos vertices
+        const vertices = this.adjList.keys();
+
+        // Iterar sobre os vertices
+        for (let vertice of vertices) {
+            //
+            const arestas = this.adjList.get(vertice);
+
+            for (let aresta of arestas) {
+                console.log(`${vertice}---->${aresta}`);
+            }
+        }
+    }
+
+    bfs(noInicial) {
+        let visitados = {};
+
+        let fila = new Array();
+        visitados[noInicial] = true;
+        fila.unshift(noInicial);
+
+        while (fila.length != 0) {
+            const elementoDaFila = fila.pop();
+            console.log(`Número: ${elementoDaFila}`);
+            const vizinhos = this.adjList.get(elementoDaFila);
+            for(let vizinho in vizinhos) {
+                if (!visitados[vizinho]) {
+                    visitados[vizinho] = true;
+                    fila.unshift(vizinho);
+                }
+            } 
+        }
+    }
+}
+
+const meuGrafo = new Graph();
+meuGrafo.addVertex(0);
+meuGrafo.addVertex(1);
+meuGrafo.addVertex(2);
+
+meuGrafo.addEdge(0, 1);
+meuGrafo.addEdge(1, 2);
+
+meuGrafo.printGraph();
+
+
+
